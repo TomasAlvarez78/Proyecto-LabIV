2c93249712d4773ae6b26f80f427f6c20edefc74
Tomas Alvarez <alvareztomasignacio@gmail.com>
2021-08-09 19:53:00 -0300

    Primer Commit

---
+++ eCommerce/Carrito/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from Carrito.models import *
+#from models import Categoria
+# Register your models here.
+
+admin.site.register(Categoria)
+admin.site.register(Producto)
+admin.site.register(DetalleCompra)
---
+++ eCommerce/Carrito/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+# Create your models here.
+
+class Categoria(models.Model):
+    idCategoria     = models.BigIntegerField(primary_key=True,null=False)
+    nombre          = models.CharField(null=False,max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name='Categoria'
+        verbose_name_plural='Categorias'
+
+class Producto(models.Model):
+    idProducto  = models.BigIntegerField(primary_key=True,null=False)
+    nombre      = models.CharField(null=False,max_length=50)
+    descripcion = models.CharField(null=False,max_length=300)
+    precio      = models.FloatField()
+    cantidadStock = models.FloatField()
+    idCategoria     = models.ForeignKey(Categoria,on_delete=models.CASCADE,verbose_name='Categoria')
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name='Producto'
+        verbose_name_plural='Productos'
+
+class DetalleCompra(models.Model):
+    idProducto = models.ForeignKey(Producto,on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+    precioCompra = models.FloatField()
+
+
+
+
+
+
+
